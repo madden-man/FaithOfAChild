425b294db3219af694dd069d5abf4a8629036d4f
Tommy Madden <maddent2@aetna.com>
2019-12-07 08:34:13 -0700

    Some minor updates for success query param

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -32,28 +32,46 @@ const DonateForm = () => {
     // Create an instance of the card Element.
     const card = stripe.elements().create('card', {style});
 
-    // Add an instance of the card Element into the `card-element` <div>.
-    card.mount('#card-element');
+    if (document.getElementById('card-element')) {
+      // Add an instance of the card Element into the `card-element` <div>.
+      card.mount('#card-element');
 
 
-    // Create a token or display an error when the form is submitted.
-    const form = document.getElementById('payment-form');
-    form.addEventListener('submit', async (event) => {
-      event.preventDefault();
+      // Create a token or display an error when the form is submitted.
+      const form = document.getElementById('payment-form');
+      form.addEventListener('submit', async (event) => {
+        event.preventDefault();
 
-      const {token, error} = await stripe.createToken(card);
+        const {token, error} = await stripe.createToken(card);
 
-      if (error) {
-        // Inform the customer that there was an error.
-        const errorElement = document.getElementById('card-errors');
-        errorElement.textContent = error.message;
-      } else {
-        // Send the token to your server.
-        stripeTokenHandler(token);
-      }
-    });
+        if (error) {
+          // Inform the customer that there was an error.
+          const errorElement = document.getElementById('card-errors');
+          errorElement.textContent = error.message;
+        } else {
+          // Send the token to your server.
+          stripeTokenHandler(token);
+        }
+      });
+    }
   }, []);
   
+  const success = new URLSearchParams(window.location.search).get('success');
+
+  if (success && success === 'true') {
+    return (
+      <Layout>
+        <h1>Your donation has been received. Thank you so much for your generous support.</h1>
+      </Layout>
+    );
+  } else if (success && success === 'false') {
+    return (
+      <Layout>
+        <h1>Your donation has not been received. Something went wrong. Sorry about that!</h1>
+      </Layout>
+    )
+  }
+
   return (
     <Layout>
       <h1>Donations</h1>
@@ -61,16 +79,16 @@ const DonateForm = () => {
       <form action="http://192.168.21.77:8080" method="post" id="payment-form">
         <p style={{width: '100%'}}>100% of donations go directly to programs for a needy child.   All of our administrative costs are paid by our founders, and all overhead services (like this website) are donated.</p>
 
-        <div class="form-row">
-          <label for="card-element">
+        <div className="form-row">
+          <label htmlFor="card-element">
             Credit or debit card
           </label>
           <div id="card-element" style={{marginTop: '.5rem', marginBottom: '1rem'}} />
           <div id="card-errors" role="alert" />
-          <label for="paymentAmount">
+          <label htmlFor="paymentAmount">
             Payment Amount
-          </label>
-          <input name="paymentAmount" type="text" style={{marginBottom:'1rem'}} />
+          </label><br />
+          <input name="paymentAmount" type="text" style={{marginBottom:'1rem'}} /><span style={{marginLeft: '4px'}}>$</span>
         </div>
 
         <button>Submit Payment</button>
